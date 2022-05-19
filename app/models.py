79f0ab0cbf7bcb9c8ecdedb0587f40a63544b76e
haimy254 <haimanaare@gmail.com>
2022-05-19 11:40:40 +0300

    created table for comments

--- app/models.py
+++ app/models.py
@@ -52,4 +52,15 @@ class Blog(db.Model):
     
     def __repr__(self):
        return f'Pitch{self.description}'
+   
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(200))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    def __repr__(self):
+        return f'<Comment: {self.comment}>'
   
