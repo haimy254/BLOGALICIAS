456c2f6b153510d62ee529a4e80c22860a6f9ce1
haimy254 <haimanaare@gmail.com>
2022-05-19 16:05:00 +0300

    added routes for sign up and logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template,url_for,redirect,flash, request
 from flask_login import login_user, logout_user, login_required,current_user
 from . import auth
 from ..models import  User
-from .forms import PitchForm, RegistrationForm, LoginForm
+from .form import PitchForm, RegistrationForm, LoginForm
 from .. import db
 
 
@@ -20,3 +20,20 @@ def login():
     title = "blog login"
        
     return render_template('auth/login.html', login_form=login_form ,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
+
+@auth.route('/register', methods=["GET","POST"])
+def signUp():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data,pass_s=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    return render_template('auth/sign.html',form=form) 
