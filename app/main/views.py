58fe0fa811379adc7afba8eb8c6b600a7f40153b
haimy254 <haimanaare@gmail.com>
2022-05-20 08:23:46 +0300

    added styling on index.html

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from flask_login import current_user
 from app.main.form import  BlogForm, CommentForm
 from app.main import main
 from app import db
-from ..models import Comment
+from ..models import Comment,Blog
 
 @main.route('/')
 def index():
@@ -16,11 +16,11 @@ def blog():
     form=BlogForm()
     if form.validate_on_submit():
         blog=form.blog.data
-        blog=blog(blog=blog,user_id=current_user.id)
+        blog=Blog(blog=blog,user_id=current_user.id)
         db.session.add(blog)
         db.sesseion.commit()
         return redirect(url_for('main.blog'))
-    return render_template('index.html', form=form)
+    return render_template('blog.html', form=form)
 
 @main.route('/comment', methods= ["GET","POST"])
 def comment():
