f0fccddb3faaf40285adc08aea63923fd42e87e9
haimy254 <haimanaare@gmail.com>
2022-05-21 02:05:30 +0300

    displayed blog

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 
 
-from flask import redirect, render_template,url_for,flash
+from flask import redirect, render_template,url_for
 from flask_login import current_user
 from app.main.form import  BlogForm, CommentForm
 from app.main import main
@@ -9,36 +9,27 @@ from ..models import Comment,Blog
 
 @main.route('/')
 def index():
-    blogs=Blog.query.all
+    blogs=Blog.query.all()
     return render_template('index.html',blogs=blogs)
 
 @main.route('/blog', methods=["GET","POST"])
-def add_blog():
+def blog():
     form=BlogForm()
     if form.validate_on_submit():
         blog=Blog(blog=form.blog.data,user_id=current_user.id)
         db.session.add(blog)
         db.session.commit()
-        flash ('')
-        return redirect(url_for('main.add_blog'))
-    return render_template('blog.html', form=form, titile="Add new blog")
+        return redirect(url_for('main.blog'))
+    return render_template('index.html', form=form)
 
 @main.route('/comment', methods= ["GET","POST"])
-def comment(blog_id):
+def comment():
     form = CommentForm()
     if form.validate_on_submit():
-        comment = Comment(comment = form.comment.data,user_id=current_user.id)
+        comment = Comment(comment = form.comment.data)
         db.session.add(comment)
         db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('comment.html', form = form)
-
-@main.route('/blod/<init:blog_id>')
-def blog(blog_id):
-    blog = Blog.query_or_404(blog_id)
-    comments = Comment.query_all()
-    form = CommentForm()
-    return render_template('blogadd.html', blog=blog, comments=comments, form=form )
-
 
 
